e52a143a07dee490b4aa7eaa4ff434ef7f4c6672
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2017-02-26 14:29:45 +0300

    api url generation

--- CatsOnMap/APIServise.swift
+++ CatsOnMap/APIServise.swift
@@ -13,7 +13,7 @@ class APIServise
     //https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3d790e81c37781d21d0a4cbf7f563a9f&tags=cat&has_geo=&format=json&nojsoncallback=1&auth_token=72157680684632046-2372594dc9e309b7&api_sig=9a2b58395233f0950442451b1b42318e
     func  find(searchWord:String, success:( ([Any])->Void),
                failure:( (NSError)->Void)) {
-        
+        buildURL(with: searchWord)
         
     }
     func buildURL(with searchWord:String)->URL
@@ -27,7 +27,7 @@ class APIServise
         params["api_key"] = "2b2c9f8abc28afe8d7749aee246d951c"
         params["tags"] = searchWord
         params["format"] = "json"
-        params["has_geo"] = ""
+        params["has_geo"] = true
         params["nojsoncallback"] = 1
         
         params["extras"] = "geo,url_s,url_l"
@@ -37,11 +37,17 @@ class APIServise
         for (key,value) in params {
             paramsAsString.append("\(key)=\(value)&")
         }
+       
+        //эта строка может содрежать недопустимые символы для параметров ссылки.
+        //поэтому мы ее преобразуем. (ввведите в поисковике
+        //любой запрос и посмотрите, что выводится в ссылке с этим запросом
+        //вы увидите, что часть символов меняется еа вид %XX
+        paramsAsString = paramsAsString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         
         let link = serviseLink + "?" + paramsAsString
         print(link)
         
-        return URL(string: link)
+        return URL(string: link)!
     }
     
 }
--- CatsOnMapViewController.swift
+++ CatsOnMapViewController.swift
@@ -11,26 +11,21 @@ import MapKit
 
 class CatsOnMapViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        testRequest()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func testRequest(){
+        let servise = APIServise()
+        servise.find(searchWord: "котик", success: { _ in
+            
+        }) { _ in
+            
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
