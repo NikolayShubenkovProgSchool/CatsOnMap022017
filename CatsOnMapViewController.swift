3c673896f44f764b938dab52eb8401671d0d5713
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2017-02-26 12:44:43 +0300

    show clear mapview

---
+++ CatsOnMapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CatsOnMapViewController.swift
+//  CatsOnMap
+//
+//  Created by Nikolay Shubenkov on 26/02/2017.
+//  Copyright © 2017 Nikolay Shubenkov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CatsOnMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
